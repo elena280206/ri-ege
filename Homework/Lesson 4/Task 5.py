bb999dd20fc433b2d46633f9cdc1dd1dfcf0f2a0
elena280206 <el_tvs6@mail.ru>
2022-10-19 19:10:25 +0300

    Интенсивы

--- Homework/Lesson 4/Task 5.py
+++ Homework/Lesson 4/Task 5.py
@@ -2,10 +2,10 @@ a = int(input())
 b = int(input())
 c = int(input())
 com = 0
-if a % 2 == 0:
+if a > 0 :
     com = com + 1
-if b % 2 == 0:
+if b > 0:
     com = com + 1
-if c % 2 == 0:
+if c > 0:
     com = com + 1
 print(com)
