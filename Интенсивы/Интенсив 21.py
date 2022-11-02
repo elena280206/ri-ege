78bb6e6cd80442f8b0575f0da61ee7d32ff223c8
elena280206 <el_tvs6@mail.ru>
2022-11-02 17:50:00 +0300

    Интенсивы

--- Интенсивы/Интенсив 21.py
+++ Интенсивы/Интенсив 21.py
@@ -2,7 +2,7 @@ x1 = int(input())
 y1 = int(input())
 x2 = int(input())
 y2 = int(input())
-if abs(x1 - x2) == abs(y1 - y2):
+if abs(x1 - x2) == abs(y1 - y2) or x1 == x2 or y1 == y2:
     print('Yes')
 else:
     print('No')
